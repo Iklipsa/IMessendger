e9142c951dbf999ed4856fbede36e06aba0babed
Iklipsa <57861703+Golubev76ru@users.noreply.github.com>
2020-12-28 18:29:26 +0300

    true

--- ChatClientCS/Utils/BringNewElementIntoView.cs
+++ ChatClientCS/Utils/BringNewElementIntoView.cs
@@ -9,28 +9,5 @@ namespace ChatClientCS.Utils
     {
         private INotifyCollectionChanged notifier;
 
-        protected override void OnAttached()
-        {
-            base.OnAttached();
-            notifier = AssociatedObject.Items as INotifyCollectionChanged;
-            notifier.CollectionChanged += ItemsControl_CollectionChanged;
-        }
-
-        protected override void OnDetaching()
-        {
-            base.OnDetaching();
-            notifier.CollectionChanged -= ItemsControl_CollectionChanged;
-        }
-
-        private void ItemsControl_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                var newIndex = e.NewStartingIndex;
-                var newElement = AssociatedObject.ItemContainerGenerator.ContainerFromIndex(newIndex);
-                var item = (FrameworkElement)newElement;
-                item?.BringIntoView();
-            }
-        }
     }
 }
